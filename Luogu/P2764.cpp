73f06b83781711b7d581e30feaf90dd758c1bcd7
Chinese_zjc_ <1751557948@qq.com>
2020-12-30 14:02:10 +0800

    Chinese_zjc_

--- Luogu/P2764.cpp
+++ Luogu/P2764.cpp
@@ -1,32 +1,38 @@
 //This Code was made by Chinese_zjc_.
-#include<iostream>
-#include<fstream>
-#include<iomanip>
-#include<algorithm>
-#include<vector>
-#include<bitset>
-#include<cmath>
-#include<queue>
-#include<stack>
-#include<list>
-#include<string>
-#include<cstring>
-#include<cstdio>
-#include<cstdlib>
-#include<cctype>
-#include<map>
-#include<set>
-#include<ctime>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <map>
+#include <set>
+#include <ctime>
 // #define debug
 #define int long long
 #define double long double
 using namespace std;
-const double PI=acos(-1);
-const double eps=0.0000000001;
-const int INF=0x3fffffffffffffff;
+const double PI = acos(-1);
+const double eps = 0.0000000001;
+const int INF = 0x3fffffffffffffff;
+int n, m, match[155], a, b;
+vector<int> to[155];
 signed main()
 {
     ios::sync_with_stdio(false);
-    
+    cin >> n >> m;
+    for (int i = 1; i <= m; ++i)
+    {
+        cin >> a >> b;
+    }
     return 0;
 }
