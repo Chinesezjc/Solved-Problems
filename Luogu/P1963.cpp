1c8037b2643fdc20b7a72070c0d07bb1a6d342a3
Chinese_zjc_ <1751557948@qq.com>
2020-12-30 11:09:34 +0800

    Chinese_zjc_

--- Luogu/P1963.cpp
+++ Luogu/P1963.cpp
@@ -24,8 +24,25 @@ using namespace std;
 const double PI = acos(-1);
 const double eps = 0.0000000001;
 const int INF = 0x3fffffffffffffff;
-int n, a[10005];
+int n, a[10005], vis[10005], match[10005], ans[10005], cnt;
 vector<int> to[10005];
+bool dfs(const int &now, const int &tag)
+{
+    if (vis[now] == tag)
+    {
+        return false;
+    }
+    vis[now] = tag;
+    for (auto i : to[now])
+    {
+        if (!match[i] || dfs(match[i], tag))
+        {
+            match[i] = now;
+            return true;
+        }
+    }
+    return false;
+}
 signed main()
 {
     ios::sync_with_stdio(false);
@@ -38,13 +55,34 @@ signed main()
             cout << "No Answer" << endl;
             return 0;
         }
-        to[i].push_back((i - a[i] + n) % n);
-        to[i].push_back((i + a[i] + n) % n);
+        to[i].push_back((i + n - a[i]) % n);
+        to[i].push_back((i + n + a[i]) % n);
         sort(to[i].begin(), to[i].end());
         to[i].erase(unique(to[i].begin(), to[i].end()), to[i].end());
     }
+    fill(vis, vis + n, -1);
     for (int i = n - 1; i >= 0; --i)
     {
+        if (dfs(i, i))
+        {
+            ++cnt;
+        }
     }
+    if (cnt == n)
+    {
+        cout << "No Answer" << endl;
+        return 0;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        ans[match[i]] = i;
+        cout << match[i] << ' ';
+    }
+    cout << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << ans[i] << ' ';
+    }
+    cout << endl;
     return 0;
 }
