d71c02c15ead76fb64f59aa8ebbbfbec5718643d
Chinesezjc <Chinesezjc@users.noreply.github.com>
2021-01-03 18:21:18 +0800

    Chinese_zjc_

--- Luogu/P3803_FFT.cpp
+++ Luogu/P3803_FFT.cpp
@@ -24,10 +24,72 @@ using namespace std;
 const double PI = acos(-1);
 const double eps = 0.0000000001;
 const int INF = 0x3fffffffffffffff;
-int n, m;
-vector<complex<double>> A, B;
+template <typename T>
+class complex
+{
+public:
+    T x, y;
+    complex(const T xx = 0, const T yy = 0)
+    {
+        x = xx;
+        y = yy;
+    }
+    const complex operator+(const complex &__Val) const
+    {
+        return {x + __Val.x, y + __Val.y};
+    }
+    const complex operator-(const complex &__Val) const
+    {
+        return {x - __Val.x, y - __Val.y};
+    }
+    const complex operator*(const complex &__Val) const
+    {
+        return {x * __Val.x - y * __Val.y, x * __Val.y + y * __Val.x};
+    }
+    complex &operator+=(const complex &__Val)
+    {
+        return *this = *this + __Val;
+    }
+    complex &operator-=(const complex &__Val)
+    {
+        return *this = *this - __Val;
+    }
+    complex &operator*=(const complex &__Val)
+    {
+        return *this = *this * __Val;
+    }
+};
+vector<int> A, B, C;
+int n, m, len, rev[1 << 21];
+complex<double> w[1 << 21 | 1];
 signed main()
 {
     ios::sync_with_stdio(false);
+    cin >> n >> m;
+    len = 1ll << (int)ceil(log2(n + m));
+    A.resize(len);
+    B.resize(len);
+    C.resize(len);
+    w[0] = w[len] = 1;
+    for (int i = len; i; i >>= 1)
+    {
+        w[i] = {cos(PI * 2 / (i / len)), sin(PI * 2 / (i / len))};
+        for (int j = i << 1; j < len; j += i << 1)
+        {
+            w[j | i] = w[j] * w[i];
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> A[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> B[i];
+    }
+    for (int i = 1; i < len; ++i)
+    {
+        rev[i] = (rev[i >> 1] >> 1) | ((i & 1) * len >> 1);
+    }
     return 0;
 }
