9f599d9e325896f5b040aec82a4747234a4dd481
Chinese_zjc_ <1751557948@qq.com>
2021-07-11 12:26:24 +0800

    Chinese_zjc_ 2021.7.11

--- CodeForces/mkdata.cpp
+++ CodeForces/mkdata.cpp
@@ -27,22 +27,12 @@ const double eps = 0.0000000001;
 const int INF = 0x3fffffffffffffff;
 unsigned long long seed = std::chrono::system_clock::now().time_since_epoch().count() / 1000000;
 std::mt19937_64 Rand(seed);
-int T = 1000000, k = 26, a[200005], b[200005];
-int RAND(int x) { return Rand() % x; }
+int n = 200, fa[1005];
 signed main()
 {
     std::ios::sync_with_stdio(false);
-    std::cout << T << std::endl;
-    while (T--)
-    {
-        std::cout << k << std::endl;
-        for (int i = 1; i <= 3; ++i)
-        {
-            int len = 3;
-            while (len--)
-                std::cout << char(Rand() % k + 'a');
-            std::cout << std::endl;
-        }
-    }
+    std::cout << n << std::endl;
+    for (int i = 2; i <= n; ++i)
+        std::cout << i << ' ' << i - 1 << std::endl;
     return 0;
 }
